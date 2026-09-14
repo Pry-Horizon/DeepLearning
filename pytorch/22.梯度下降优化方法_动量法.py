a209a59ae7f76e4e28f03c020de00a99a60ae1b7
QuHaohao <quhao121@gmail.com>
2026-09-14 16:42:36 +0800

    Adam

--- pytorch/22.梯度下降优化方法_动量法.py
+++ pytorch/22.梯度下降优化方法_动量法.py
@@ -94,9 +94,39 @@ def dm03_prop():
     print(f'w:{w},w.grad:{w.grad}')
     print("==="*30)
 
+# 4.演示梯度下降优化方法：->Adam
+
+
+def dm04_adam():
+    # 1.初始化权重参数。
+    w = torch.tensor([1.0], requires_grad=True, dtype=torch.float)
+    # 2.定义损失函数。
+    criterion = ((w**2)/2.0)
+    # 3.创建优化器。
+    optimizer = optim.Adam(params=[w], lr=0.01, betas=(0.9, 0.999))
+    # 4.计算梯度值：梯度清零+反向传播+参数更新
+    optimizer.zero_grad()
+    criterion.sum().backward()
+    optimizer.step()
+    print(f'w:{w},w.grad:{w.grad}')
+    # 5.重复更新权重
+    criterion = ((w**2)/2.0)
+    optimizer.zero_grad()
+    criterion.sum().backward()
+    optimizer.step()
+    print(f'w:{w},w.grad:{w.grad}')
+
+    criterion = ((w**2)/2.0)
+    optimizer.zero_grad()
+    criterion.sum().backward()
+    optimizer.step()
+    print(f'w:{w},w.grad:{w.grad}')
+    print("==="*30)
+
 
 # 5.测试。
 if __name__ == '__main__':
     dm01_momentum()
     dm02_adagrad()
     dm03_prop()
+    dm04_adam()
